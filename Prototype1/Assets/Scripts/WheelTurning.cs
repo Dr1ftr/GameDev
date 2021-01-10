d4e4fded4f2fbd68212ac85418a676aeb3ab6464
Dr1ft3r <331516@student.mboutrecht.nl>
2021-01-10 14:48:48 +0100

    Addition: Protype 1
    Type: Addition
    Why: Homework

---
+++ Prototype1/Assets/Scripts/WheelTurning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelTurning : MonoBehaviour
+{
+    public float turnSpeed;
+
+    public float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
